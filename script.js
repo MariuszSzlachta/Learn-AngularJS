5ea1bf41baf5d250023fd775e10b0f48a73b39ce
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-01-29 12:40:51 +0100

    Use object properities in scope

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const myController = myApp.controller("myController", function($scope){
   $scope.message = 'AngularJS tutorial';
   console.log($scope.message)
 })
+
+const employeeController = function($scope){
+  $scope.employee = {
+    name: 'John',
+    gender: 'male'
+  }
+}
+
+myApp.controller('employeeController', employeeController);
