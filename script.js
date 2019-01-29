5bb02701f5f2fe434002e73eef9b35bb0aaf5f81
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-01-29 13:41:34 +0100

    Create like/dislike table and handle clicks in angularJS

--- script.js
+++ script.js
@@ -2,81 +2,19 @@ const myApp = angular.module('myModule', []);
 
 
 const dataController = myApp.controller('dataController', $scope => {
-  const employees = [{
-      firstName: "Ben",
-      lastName: "Hastings",
-      gender: "Male",
-      salary: 55000
-    },
-    {
-      firstName: "Sara",
-      lastName: "Paul",
-      gender: "Female",
-      salary: 68000
-    },
-    {
-      firstName: "Mark",
-      lastName: "Holland",
-      gender: "Male",
-      salary: 57000
-    },
-    {
-      firstName: "Pam",
-      lastName: "Macintosh",
-      gender: "Female",
-      salary: 53000
-    },
-    {
-      firstName: "Todd",
-      lastName: "Barber",
-      gender: "Male",
-      salary: 60000
-    }
-  ];
+  const technologies = [
+    { name: "C#", likes: 0, dislikes: 0 },
+    { name: "ASP.NET", likes: 0, dislikes: 0 },
+    { name: "SQL", likes: 0, dislikes: 0 },
+    { name: "AngularJS", likes: 0, dislikes: 0 }
+  ]
+  $scope.technologies = technologies;
 
-  $scope.employees = employees;
-})
+  $scope.incrementLikes = function(technology) {
+    technology.likes++;
+  }
 
-const countriesController = myApp.controller('countriesController', $scope => {
-  const countries = [{
-      name: "UK",
-      cities: [{
-          name: "London"
-        },
-        {
-          name: "Birmingham"
-        },
-        {
-          name: "Manchester"
-        }
-      ]
-    },
-    {
-      name: "USA",
-      cities: [{
-          name: "Los Angeles"
-        },
-        {
-          name: "Chicago"
-        },
-        {
-          name: "Houston"
-        }
-      ]
-    },
-    {
-      name: "India",
-      cities: [{
-          name: "Hyderabad"
-        },
-        {
-          name: "Chennai"
-        },
-        {
-          name: "Mumbai"
-        }
-      ]
-    }
-  ];
-$scope.countries = countries;
+  $scope.incrementDislikes = function(technology) {
+    technology.dislikes++
+  }
 })
