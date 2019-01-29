bd8598dafc9ac9ee1ccd72c1a33df3fddfe8b870
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-01-29 18:45:29 +0100

    Build custom filter rule

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ const dataController = myApp.controller('dataController', $scope => {
   const employees = [
     {
       name: "Ben", dateOfBirth: new Date("November 23, 1980"),
-      gender: "Male", salary: 55000.788
+      gender: "Male", salary: 55000.788, city: 'New York'
     },
     {
       name: "Sara", dateOfBirth: new Date("May 05, 1970"),
-      gender: "Female", salary: 68000
+      gender: "Female", salary: 68000, city: 'London'
     },
     {
       name: "Mark", dateOfBirth: new Date("August 15, 1974"),
-      gender: "Male", salary: 57000
+      gender: "Male", salary: 57000, city: 'New York'
     },
     {
       name: "Pam", dateOfBirth: new Date("October 27, 1979"),
-      gender: "Female", salary: 53000
+      gender: "Female", salary: 53000, city: 'London'
     },
     {
       name: "Todd", dateOfBirth: new Date("December 30, 1983"),
-      gender: "Male", salary: 60000
+      gender: "Male", salary: 60000, city: 'Paris'
     }
   ];
 
@@ -41,5 +41,16 @@ const dataController = myApp.controller('dataController', $scope => {
       return $scope.reverseSort ? 'arrow-down' : 'arrow-up'
     }
     return '';
+  }
+
+  $scope.search = item => {
+    if ($scope.searchText === undefined){
+      return true
+    } else {
+     if (item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 || item.city.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1  ){
+        return true
+      }
+    }
+    return false
   }
 })
