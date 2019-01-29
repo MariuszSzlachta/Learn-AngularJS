e501812eedfc35680195839647d52b7fd3ce2f85
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-01-29 12:47:34 +0100

    Learn ng-src directive

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 const myApp = angular.module('myModule', []);
 
-const myController = myApp.controller("myController", function($scope){
-  $scope.message = 'AngularJS tutorial';
-  console.log($scope.message)
-})
-
-const employeeController = function($scope){
-  $scope.employee = {
-    name: 'John',
-    gender: 'male'
+const imageController = myApp.controller('imageController', function($scope){
+  const image = {
+    name: 'placeholder',
+    url: '/pic.png'
   }
-}
-
-myApp.controller('employeeController', employeeController);
+  $scope.image = image;
+})
