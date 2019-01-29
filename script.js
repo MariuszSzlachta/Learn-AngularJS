5889b39613de0f1e8a6da240127afb0f2c91ddad
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-01-29 13:03:13 +0100

    Learn ng-repeat directive

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const myApp = angular.module('myModule', []);
 
-const imageController = myApp.controller('imageController', function($scope){
-  const image = {
-    name: 'placeholder',
-    url: '/pic.png'
-  }
-  $scope.image = image;
-})
 
 const dataController = myApp.controller('dataController', $scope => {
-  $scope.message = 'hello world';
+  const employees = [
+    { firstName: "Ben", lastName: "Hastings", gender: "Male", salary: 55000 },
+    { firstName: "Sara", lastName: "Paul", gender: "Female", salary: 68000 },
+    { firstName: "Mark", lastName: "Holland", gender: "Male", salary: 57000 },
+    { firstName: "Pam", lastName: "Macintosh", gender: "Female", salary: 53000 },
+    { firstName: "Todd", lastName: "Barber", gender: "Male", salary: 60000 }
+  ];
+
+  $scope.employees = employees;
 })
+
